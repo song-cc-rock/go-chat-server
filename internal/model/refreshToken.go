47684026da545a4bfb4dfffc085d3a0562aa5d16
song-cc-rock <changchang.song@fit2cloud.com>
2025-09-13 23:58:18 +0800

    feat：Add refresh token

---
+++ internal/model/refreshToken.go
@@ -0,0 +1,11 @@
+package model
+
+type UserRefreshToken struct {
+	ID           string `gorm:"type:varchar(50);not null;primarykey"`   // ID
+	UserID       string `gorm:"type:varchar(50);not null;index"`        // 所属用户
+	RefreshToken string `gorm:"type:varchar(255);not null;uniqueIndex"` // refresh token，本质是随机字符串或JWT
+	ExpiresAt    int64  `gorm:"type:bigint;not null"`                   // 过期时间
+	Revoked      bool   `gorm:"default:false"`                          // 是否已失效（用户登出/管理员强制下线）
+	CreatedAt    int64  `gorm:"type:bigint;not null;"`                  // 创建时间
+	UpdatedAt    int64  `gorm:"type:bigint;not null;"`                  // 更新时间
+}
